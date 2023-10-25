685ae29b840fd8e1807af80d554df801e9b1167a
ewdr12333 <prasika50@gmail.com>
2023-10-25 10:53:20 +0530

    created project structure

---
+++ opencartapplication/src/main/java/testBase/TestBaseClasses.java
@@ -0,0 +1,15 @@
+package testBase;
+
+public class TestBaseClasses<WebDriver> {
+	
+	
+	
+	
+	public void testbases(WebDriver driver)
+	{
+		driver=new ChromeDriver();
+      String url="";
+      
+	}
+
+}
